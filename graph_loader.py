28a1ba0735d2f80c5214c90e7998467c0f232fcb
JonasM <jonas.maurer@uni-a.de>
2025-01-15 00:20:26 +0100

    slightly change loading of graphs

--- graph_loader.py
+++ graph_loader.py
@@ -1,16 +1,9 @@
 import pickle
 
-def read_dat_file(filepath):
+def load_graphs(filepath):
     try:
         with open(filepath, 'rb') as file:
             data = pickle.load(file)
         return data
     except Exception as e:
         return f"Error reading file: {e}"
-
-
-
-
-if __name__ == '__main__':
-    content = read_dat_file('data/graphs.dat')
-    print(content)
